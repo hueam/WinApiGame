eff375bf4ce8375fc667abcc081e29cbb032aefc
ye0jin <fruitfighterlim@gmail.com>
2023-11-26 14:17:50 +0900

    inven

---
+++ 2023_winapi_framework/Inventory.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Item.h"
+
+class Inventory
+{
+public:
+	Inventory();
+	~Inventory();
+	
+public:
+	void SetInventory(int size);
+
+public:
+	void SelectItem(const Item& item);
+	void UseItem(int idx);
+	//void UseItem(const Item& item);
+
+private:
+	vector<Item> invenItems; // or using map
+	int itemCnt = 0; // current item cnt
+};
+
