c2322c01561769c84e9963645c97dd21bc97335f
ye0jin <fruitfighterlim@gmail.com>
2023-11-28 10:31:39 +0900

    inven2

---
+++ 2023_winapi_framework/Inventory.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "Inventory.h"
+
+Inventory::Inventory()
+{
+}
+
+Inventory::~Inventory()
+{
+	invenItems.clear();
+}
+
+void Inventory::SetInventory(int size)
+{
+	//
+}
+
+void Inventory::SelectItem(const Item& item)
+{
+	// if() item->itemType != END
+	invenItems.push_back(item);
+	SetInventory(++itemCnt);
+}
+
+void Inventory::UseItem(int idx) // add item in the inven Index
+{
+	if (idx >= 0 && idx < itemCnt)
+	{
+		invenItems.erase(invenItems.begin() + idx);
+		SetInventory(--itemCnt);
+	}
+}
+
+//void Inventory::UseItem(const Item& item)
+//{
+//	auto iter = std::find(items.begin(), items.end(), item); //find item
+//	if (iter != items.end())
+//	{
+//		items.erase(iter); // erase
+//	}
+//}
