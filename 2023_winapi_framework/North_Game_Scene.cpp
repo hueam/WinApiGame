ba432f5174017006698c714abb5c43a3cda2559d
ye0jin <fruitfighterlim@gmail.com>
2023-12-05 11:22:01 +0900

    texture

--- 2023_winapi_framework/North_Game_Scene.cpp
+++ 2023_winapi_framework/North_Game_Scene.cpp
@@ -5,20 +5,15 @@
 #include "Core.h"
 #include "SceneUI.h"
 #include "CollisionMgr.h"
-#include "TestItem.h"
+#include "Window.h"
 #include "CameraMgr.h"
 
 void North_Game_Scene::Init()
 {
 	SceneUI* ui = GetSceneUI();
 	CameraMgr::GetInst()->Init();
-	Object* pObj = new TestItem();
+	Object* pObj = new Window();
 	pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2 })));
-	pObj->SetScale(Vec2(100.f, 100.f));
+	pObj->SetScale(Vec2(450.f, 400.f));
 	AddObject(pObj);
-	pObj = new TestItem();
-	pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2+100, Core::GetInst()->GetResolution().y / 2 })));
-	pObj->SetScale(Vec2(100.f, 100.f));
-	AddObject(pObj);
-
 }
