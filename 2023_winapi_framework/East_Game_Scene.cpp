427b8594effe78eb9363dbf4a884317c4559fe94
ye0jin <fruitfighterlim@gmail.com>
2023-12-12 11:41:53 +0900

    ㅁㄴㅇㄹ

--- 2023_winapi_framework/East_Game_Scene.cpp
+++ 2023_winapi_framework/East_Game_Scene.cpp
@@ -11,18 +11,18 @@
 
 void East_Game_Scene::Init()
 {
-	Object* pObj = new UnusedObj(L"board");
-	pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2 - 250, Core::GetInst()->GetResolution().y / 2 - 40})));
-	pObj->SetScale(Vec2(560.f, 320.f));
+	Object* pObj = new UnusedObj(L"small_board");
+	pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2 + 100, Core::GetInst()->GetResolution().y / 2 - 70 })));
+	pObj->SetScale(Vec2(195.f, 320.f));
 	AddObject(pObj, RENDER_ORDER::ONE);
-	
-	pObj = new UnusedObj(L"small_board");
-	pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2 + 102, Core::GetInst()->GetResolution().y / 2 - 50})));
-	pObj->SetScale(Vec2(205.f, 320.f));
+
+	pObj = new UnusedObj(L"board");
+	pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2 - 250, Core::GetInst()->GetResolution().y / 2 - 70})));
+	pObj->SetScale(Vec2(560.f, 320.f));
 	AddObject(pObj, RENDER_ORDER::ONE);
 
 	pObj = new UnusedObj(L"teachingdesk");
-	pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2, Core::GetInst()->GetResolution().y / 2 })));
+	pObj->SetPos((Vec2({ Core::GetInst()->GetResolution().x / 2 - 350, Core::GetInst()->GetResolution().y / 2 + 180})));
 	pObj->SetScale(Vec2(250.f, 350.f));
 	AddObject(pObj, RENDER_ORDER::ONE);
 }
